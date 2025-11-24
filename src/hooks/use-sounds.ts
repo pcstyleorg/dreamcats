2011d59c1fd43e9e5490c2397b8e1fd7b4ee4d46
pc-style <pcstyle@duck.com>
2025-11-24 14:25:22 +0100

    refactor(tutorial): move tutorial state to Zustand store and keep legacy context API

--- src/hooks/use-sounds.ts
+++ src/hooks/use-sounds.ts
@@ -2,6 +2,8 @@ import { useCallback } from 'react';
 
 // Disable sounds for now since CDN is blocking requests
 // We'll use silent placeholders to prevent errors
+// keep this block for bundler side-effects; not used directly in the hook
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const soundFiles = {
   flip: '',
   draw: '',
@@ -18,8 +20,10 @@ export type SoundType = keyof typeof soundFiles;
 // TODO: Add local sound files to /public/sounds/ directory
 
 export const useSounds = () => {
-  const playSound = useCallback((_sound: SoundType) => {
+  const playSound = useCallback((sound: SoundType) => {
     // Sounds disabled - CDN blocking requests
+    // Keep parameter to satisfy interface, but don't use it.
+    void sound;
     return;
   }, []);
 
